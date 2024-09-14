c3334b7346156db63973cc22064a55534a2600b1
BewarPiri <122524395+BewarPiri@users.noreply.github.com>
2024-09-15 00:57:01 +0200

    filter out the movies instead of new fetch.

--- Frontend/src/MovielistPage.jsx
+++ Frontend/src/MovielistPage.jsx
@@ -5,7 +5,7 @@ import { FavmovieCard } from "./components/MovieCard/FavmoviecardComponent";
 import { Nav } from "./components/navComponent/NavComponent";
 
 function MovielistPage() {
-  const [movies, setMovies] = useState([]); // state for å lagre movie data 
+  const [movies, setMovies] = useState([]); // state for å lagre movie data
   const [error, setError] = useState(null); // State for å lage errors
   const [loading, setLoading] = useState(true); // State for å håndtere loading
 
@@ -19,13 +19,14 @@ function MovielistPage() {
         throw new Error("Network response was not ok");
       }
       const data = await response.json(); // Parse the JSON response
-      console.log("Fetched data:", data);  // Log the fetched data
-  
+      console.log("Fetched data:", data); // Log the fetched data
+
       // parse movielist fra en string til et array
-      const movieList = typeof data.movieList === 'string'
-        ? JSON.parse(data.movieList)
-        : data.movieList;
-  
+      const movieList =
+        typeof data.movieList === "string"
+          ? JSON.parse(data.movieList)
+          : data.movieList;
+
       setMovies(movieList); // Set the movies state with the parsed array
       console.log("movie set: ", movieList); // Log the movies state
     } catch (error) {
@@ -35,48 +36,53 @@ function MovielistPage() {
       setLoading(false); // Set loading to false
     }
   };
-  
 
   // useEffect to fetch data when the component mounts
   useEffect(() => {
     fetchMovies();
   }, []);
 
-  const removeFromWatchlist = async(imdbID) => {
+  const removeFromWatchlist = async (imdbID) => {
     try {
       const response = await fetch(`http://localhost:3000/ap/movielist/`, {
-        method: 'DELETE',
+        method: "DELETE",
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
         },
         body: JSON.stringify({ imdbID }), // Sending movie in the request body
       });
 
       if (!response.ok) {
-        throw new Error('Failed to remove movie from movielist');
+        throw new Error("Failed to remove movie from movielist");
       }
-      
-    } catch (error) {
-      console.error('Error adding to watchlist:', error);
-      alert('An error occurred while trying to delete a movie from your list');
-    }
 
-    fetchMovies();
+      //oppdater staten direkte ved å filtrere ut de slettede filmene(da slipper vi å gjøre en ny fetch request)
+      setMovies((previousMovies) =>
+        previousMovies.filter((movie) => movie.imdbid !== imdbID)
+      );
+    } catch (error) {
+      console.error("Error adding to watchlist:", error);
+      alert("An error occurred while trying to delete a movie from your list");
+    }
   };
 
-
-    // Check if movies are being correctly set
-    console.log("Movies state:", movies); 
+  // Check if movies are being correctly set
+  console.log("Movies state:", movies);
 
   // vis enten loading, error, eller movies
   return (
     <div>
-    <div className="pb-5" data-theme="valentine">
-    <Nav/>
-    <h1 className="text-6xl font-extrabold text-center py-6" data-theme="valentine">Favorite Movie List</h1>
-    </div>
+      <div className="pb-5" data-theme="valentine">
+        <Nav />
+        <h1
+          className="text-6xl font-extrabold text-center py-6"
+          data-theme="valentine"
+        >
+          Favorite Movie List
+        </h1>
+      </div>
 
-      {loading && <p>Loading...</p>} 
+      {loading && <p>Loading...</p>}
       {error && <p>Error: {error}</p>}
 
       {/* Display the movie list */}
@@ -84,7 +90,7 @@ function MovielistPage() {
         {movies.length > 0 ? (
           <div className="grid grid-cols-5 gap-6 w-full">
             {movies.map((movie) => (
-              <FavmovieCard 
+              <FavmovieCard
                 key={movie.imdbid} // Update key to use imdbid
                 movie={{
                   title: movie.title,
